a08b6ef87460acaadc1f503bdf0960013ae44677
campus <campus@S31902>
2018-10-27 10:27:31 +0300

    project C# created

---
+++ Xml/Xml/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XML");
+            Console.ReadKey();
+        }
+    }
+}
